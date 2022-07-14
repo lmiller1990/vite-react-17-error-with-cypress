15717584aeabfae46f815c9cbebeac4473e2e57c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-14 23:55:47 +1000

    simplify

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import react from '@vitejs/plugin-react'
-
-const debug = console.log
-
-let shouldTransform = false
-try {
-  const react = require('react')
-  shouldTransform = react.version.startsWith('17')
-} catch (e) {
-  // not using react
-}
-
-function plugin () {
-  return {
-    enforce: 'pre',
-    transform: (code, id) => {
-      if (shouldTransform && id.includes('cypress_react.js')) {
-        return code.replace('react-dom/client', 'react-dom')
-      }
-    }
-  }
-}
-
-const exclude = []
-
-if (shouldTransform) {
-  exclude.push('react-dom/client')
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [plugin(), react()],
-  optimizeDeps: {
-    exclude
-  }
+  plugins: [react()],
 })
